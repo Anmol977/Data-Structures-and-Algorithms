894cce587c931c170382c3dab28062836eab1e61
Anmol Chauhan <anmolchauhan977@gmail.com>
2021-12-19 14:04:29 +0530

    Add files via upload

--- Data Structures/Heap/heap.cpp
+++ Data Structures/Heap/heap.cpp
@@ -38,14 +38,61 @@ public:
                     parent = pos / 2;
                }
           }
-          std::cout<<"inserted "<<data<<std::endl;
+          std::cout << "inserted " << data << std::endl;
+     }
+     t top()
+     {
+          return heap[1];
+     }
+     void pop()
+     {
+          int last = heap.size() - 1;
+          std::swap(heap[1], heap[last]);
+          heap.pop_back();
+          heapify(1);
+     }
+     void heapify(int idx)
+     {
+          int left = 2 * idx, right = 2 * idx+ 1;
+          int last = heap.size() - 1;
+          if (isminheap)
+          {
+               if (heap[idx] > heap[left] and left <= last)
+               {
+                    std::swap(heap[idx], heap[left]);
+               }
+               if (heap[idx] > heap[right] and right <= last)
+               {
+                    std::swap(heap[idx], heap[right]);
+               }
+            
+          } else {
+               if (heap[idx] < heap[left] and left <= last)
+               {
+                    std::swap(heap[idx], heap[left]);
+               }
+               if (heap[idx] < heap[right] and right <= last)
+               {
+                    std::swap(heap[idx], heap[right]);
+               }
+          }
+     }
+     bool empty(){
+          return heap.size() == 1;
      }
 };
 
-int main(){
+int main()
+{
      Heap<int> h1;
-     h1.insert(1);
-     h1.insert(2);
-     h1.insert(3);
-     h1.insert(4);
+     h1.insert(10);
+     h1.insert(20);
+     h1.insert(15);
+     h1.insert(14);
+     h1.insert(11);
+     h1.insert(90);
+     while(!h1.empty()){
+          std::cout<<h1.top()<<std::endl;
+          h1.pop();
+     }
 }
